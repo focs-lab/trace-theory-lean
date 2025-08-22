ab9f2c4bdb305ce0342a8480f2af47b4c949ba6a
jefrai <30693850+jefrai@users.noreply.github.com>
2025-08-22 17:11:40 +0800

    Add notes on Monoid & Trace Equivalence (for self-reference)

--- TraceTheoryLean/Main.lean
+++ TraceTheoryLean/Main.lean
@@ -3,31 +3,54 @@ import Mathlib.Data.Finset.Basic
 import Mathlib.Data.List.Basic
 import Mathlib.Computability.Language
 import Mathlib.Logic.Relation
+import Mathlib.Data.Set.Basic
 
-variable {U : Type} [DecidableEq U]
+variable {α : Type*} [DecidableEq α]
+-- An alphabet is a finite set of symbols / letters
+def Alphabet α := Finset α
+instance : Membership α (Alphabet α) := Finset.instMembership
 
-def Alphabet U := Finset U
+-- Implement string structure w/ lists as underlying
 
-def proj (S : Finset U) (w : List U) : List U :=
+-- e.g. w/ length, etc.
+-- notation : "|" s : string "|"
+-- def concat :
+-- w/ ∘ notation
+-- notation : ε = []
+
+-- def occurs w a : Bool
+-- <-> a ∈ w
+
+-- def Alph w : Alphabet α := {x | x ∈ w}
+
+-- [DecidablePred (· ∈ S)] just once?
+def proj (S : Alphabet α) [DecidablePred (· ∈ S)] (w : List α) : List α :=
   match w with
   | [] => []
   | b :: u => if b ∈ S then b :: proj S u else proj S u
 
-def cancel (w : List U) (a : U) :=
+def cancel (w : List α) (a : α) :=
   match w with
   | [] => []
   | b :: u => if a = b then u else b :: (cancel u a)
 
-lemma cancelling_proj_mem_is_id (S : Finset U) (w : List U) (a : U) :
+-- cons list -> induction w with | cons u b IH
+
+-- Lemma for [proj_cancel_comm]:
+-- Cancelling with an element not in the set S does not change the projection
+lemma cancelling_proj_mem_is_id (S : Finset α) (w : List α) (a : α) :
   (a ∉ S) -> (cancel (proj S w) a = proj S w) := by
   intro h
+  -- Structural induction on strings as lists
   induction w with
   | nil => rfl
   | cons b u IH =>
     by_cases h_ab : a = b
+    -- Case where a = b
     · rw [h_ab] at h
       simp [proj, h]
       exact IH
+    -- Case where a ≠ b
     · by_cases h_bs : b ∈ S
       · simp [proj, h_bs]
         simp [cancel, h_ab]
@@ -35,7 +58,7 @@ lemma cancelling_proj_mem_is_id (S : Finset U) (w : List U) (a : U) :
       · simp [proj, h_bs]
         exact IH
 
-lemma proj_cancel_comm (S : Finset U) (w : List U) (a : U) : -- (1.3)
+lemma proj_cancel_comm (S : Finset α) (w : List α) (a : α) : -- (1.3)
   proj S (cancel w a) = cancel (proj S w) a := by
   induction w with
   | nil => rfl
@@ -57,12 +80,23 @@ lemma proj_cancel_comm (S : Finset U) (w : List U) (a : U) : -- (1.3)
         simp [proj, h_bs]
         exact IH
 
-def proj_lang (S : Finset U) (A : Language U) : Language U :=
+def proj_lang (S : Finset α) (A : Language α) : Language α :=
   Set.image (proj S) A
 
-def pref (w : List U) : Language U := {u : List U | ∃v, u ++ v = w}
+def pref (w : List α) : Language α := {u : List α | ∃v, u ++ v = w}
+
+-- see [Preorder] class
 
 structure Dependency (α) [Fintype α] where
   r : α → α → Prop
   refl : ∀ x, r x x
   symm : ∀ {x y}, r x y → r y x
+
+
+-- def trace_equivalence (D) :
+-- such that
+  -- {([x], [y]) | (x, y) ∈ I_D}
+  -- follows monoid structure (axioms)
+  -- transitively closed
+  --
+  -- -> (optional? prove is least congruence)
