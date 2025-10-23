687cc992b8eed791302d8baeb35730b8b2f91eee
yisiox <yisiong.y22@gmail.com>
2025-10-23 22:08:52 +0800

    Init DependenceGraph.lean

---
+++ TraceTheory/TraceTheory/DependenceGraph.lean
@@ -0,0 +1,26 @@
+import Mathlib.Logic.Relation
+import TraceTheory.Trace
+
+open Trace
+
+variable {α : Type*} [DecidableEq α]
+variable {D : Dependence α}
+
+variable (D) in
+structure DependenceGraph where
+  V : Type
+  [fintype : Fintype V]
+  R : V → V → Prop
+  φ : V → α
+  acyclic : ∀ v, ¬ Relation.TransGen R v v
+  d_conn : ∀ v₁ v₂, R v₁ v₂ ∨ R v₂ v₁ ∨ v₁ = v₂ ↔ D.rel (φ v₁) (φ v₂)
+
+namespace DependenceGraph
+
+structure Iso (γ₁ γ₂: DependenceGraph D) where
+  toEquiv : γ₁.V ≃ γ₂.V
+  preserves_label' : ∀ v, γ₁.φ v = γ₂.φ (toEquiv v)
+  preserves_arcs' : ∀ v₁ v₂, γ₁.R v₁ v₂ ↔ γ₂.R (toEquiv v₁) (toEquiv v₂)
+
+instance (γ₁ γ₂ : DependenceGraph D) : CoeFun (γ₁.Iso γ₂) (fun _ => γ₁.V → γ₂.V) where
+  coe f := f.toEquiv.toFun
