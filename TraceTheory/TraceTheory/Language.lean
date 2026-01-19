a9b1070bb0100613bb2c9eb2750c2203ec2f4268
yisiox <yisiong.y22@gmail.com>
2026-01-19 21:20:20 +0800

    Complete proof of characterization of LexNF

--- TraceTheory/TraceTheory/Language.lean
+++ TraceTheory/TraceTheory/Language.lean
@@ -24,7 +24,7 @@ def SatisfiesFactorCondition (I : Independence α) (x : List α) : Prop :=
 
 lemma factorCondition_of_lexNf (I : Independence α) (x : List α) (h : IsLexNf I x) :
     SatisfiesFactorCondition I x := by
-  intro y u z a b hx h_indep h_lt
+  intro y u z a b hx h_indep hlt
   contrapose! h
   unfold IsLexNf
   push_neg
@@ -47,12 +47,87 @@ lemma factorCondition_of_lexNf (I : Independence α) (x : List α) (h : IsLexNf 
     apply List.append_left_lt
     apply List.cons_lt_cons_iff.mpr
     left
-    exact h_lt
+    exact hlt
+
+lemma exists_decomp_of_lt_of_len_eq {w x : List α} (hlt : w < x) (h_len : w.length = x.length):
+    ∃ p a b w' x',
+      w = p ++ [a] ++ w' ∧
+      x = p ++ [b] ++ x' ∧
+      a < b := by
+  induction w generalizing x with
+  | nil =>
+    cases x <;> contradiction
+  | cons a w' ih =>
+    cases x with
+    | nil =>
+      contradiction
+    | cons b x' =>
+      cases hlt with
+      | rel hab =>
+        use [], a, b, w', x'
+        exact ⟨rfl, rfl, hab⟩
+      | cons h_lex =>
+        simp at h_len
+        have ⟨p', a', b', w'', x'', hw', hx', hlt'⟩ := ih (List.lex_lt.mp h_lex) h_len
+        use a :: p', a', b', w'', x''
+        simp [hw', hx', hlt']
+
+lemma leftmost_occurrence {w : List α} {a : α} (h : a ∈ w) :
+    ∃ w' w'', w = w' ++ [a] ++ w'' ∧ a ∉ w' := by
+  induction w with
+  | nil =>
+    contradiction
+  | cons b v ih =>
+    by_cases hab : a = b
+    · use [], v
+      simp [hab]
+    · simp [hab] at h
+      have ⟨w', w'', h_concat, h_in⟩ := ih h
+      use [b] ++ w', w''
+      simp [h_concat, h_in, hab]
+
+lemma indep_and_decomp_of_equiv_of_head_ne {a b : α} {w x : List α}
+    (I : Independence α) (h : TraceEquiv I ([a] ++ w) ([b] ++ x)) (hne : a ≠ b) :
+    I.rel a b ∧ ∃ u v, x = u ++ [a] ++ v ∧ independent I [a] u := by
+  have h_rev := mirror_rule h
+  simp at h_rev
+  have ⟨h_indep, w_rev', _, hx_rev⟩ := indep_and_decomp_of_equiv_of_tail_ne h_rev hne
+  constructor
+  · exact h_indep
+  · have ha := (mem_iff_mem a hx_rev).mpr
+    simp at ha
+    have ⟨u, v, hx⟩ := leftmost_occurrence ha
+    use u, v
+    constructor
+    · exact hx.left
+    · rw [hx.left] at hx_rev
+      simp only [List.reverse_append, List.reverse_cons] at hx_rev
+      simp only [List.reverse_nil, List.nil_append] at hx_rev
+      rw [← List.append_assoc] at hx_rev
+      have h_mem_rev : a ∉ u.reverse := by
+        rw [List.mem_reverse]
+        exact hx.right
+      have h_indep_rev := indep_of_equiv_rightmost_symbol hx_rev h_mem_rev
+      simp [List.mem_reverse] at h_indep_rev ⊢
+      exact h_indep_rev
 
 lemma lexNf_of_factorCondition
     (I : Independence α) (x : List α) (h : SatisfiesFactorCondition I x) :
     IsLexNf I x := by
-  sorry
+  unfold IsLexNf
+  by_contra! h_not_nf
+  have ⟨w, h_equiv, hlt⟩ := h_not_nf
+  have ⟨p, a, b, w', x', hw, hx, hlt'⟩ :=
+    exists_decomp_of_lt_of_len_eq hlt (length_eq_of_equiv h_equiv).symm
+  rw [hw, hx, List.append_assoc, List.append_assoc] at h_equiv
+  replace h_equiv := (equiv_cancel_left h_equiv).symm
+  have hne := ne_of_lt hlt'
+  have ⟨h_indep, u, v, hx', hu⟩ := indep_and_decomp_of_equiv_of_head_ne I h_equiv hne
+  unfold SatisfiesFactorCondition at h
+  simp only [hx', ← List.append_assoc] at hx
+  have ⟨c, hc_mem, hc_indep⟩ := h p u v a b hx h_indep hlt'
+  simp at hu
+  exact hc_indep (hu c hc_mem)
 
 theorem isLexNf_iff_factorCondition (I : Independence α) (x : List α) :
     IsLexNf I x ↔ SatisfiesFactorCondition I x := by
