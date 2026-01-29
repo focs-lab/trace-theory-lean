7d88fb1ada9a3c11c84c6eaf3bbb34440f0c3af1
yisiox <yisiong.y22@gmail.com>
2026-01-29 15:54:53 +0800

    Remove breaking import

--- TraceTheory/TraceTheory.lean
+++ TraceTheory/TraceTheory.lean
@@ -1,4 +1,1 @@
--- This module serves as the root of the `TraceTheory` library.
--- Import modules here that should be built as part of the library.
 import TraceTheory.Basic
-import TraceTheory.Trace
