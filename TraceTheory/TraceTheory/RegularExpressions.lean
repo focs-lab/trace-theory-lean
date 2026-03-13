7190aed3ab66f4d100ad3558f9cff2040040f687
jefrai <30693850+jefrai@users.noreply.github.com>
2026-03-13 13:40:22 +0800

    Clean RegularExpressions

--- TraceTheory/TraceTheory/RegularExpressions.lean
+++ TraceTheory/TraceTheory/RegularExpressions.lean
@@ -10,36 +10,36 @@ variable {α : Type} {I : Independence α}
 def isStarConnected (I : Independence α) : RegularExpression α → Prop
   | 0 => True
   | 1 => True
-  | RegularExpression.char _ => True
+  | char _ => True
   | P + Q => isStarConnected I P ∧ isStarConnected I Q
   | P * Q => isStarConnected I P ∧ isStarConnected I Q
-  | RegularExpression.star P => isStarConnected I P ∧ (∀ s ∈ P.matches', @isConnected' α I s)
+  | star P => isStarConnected I P ∧ (∀ s ∈ P.matches', @isConnected' α I s)
 
 -- Interpretation of this RegularExpression as operating on trace languages.
 def matches_trace (I : Independence α) : RegularExpression α → Set (Trace I)
   | 0 => {}
   | 1 => {⟦[]⟧}
-  | RegularExpression.char a => {⟦[a]⟧}
+  | char a => {⟦[a]⟧}
   | P + Q => (matches_trace I P) ∪ (matches_trace I Q)
   | P * Q => {t | ∃ p : (matches_trace I P), ∃ q : (matches_trace I Q), t = p * q}
-  | RegularExpression.star P => kstar (matches_trace I P)
+  | star P => kstar (matches_trace I P)
 
 def isStarConnected_trace (I : Independence α) : RegularExpression α → Prop
   | 0 => True
   | 1 => True
-  | RegularExpression.char _ => True
+  | char _ => True
   | P + Q => isStarConnected I P ∧ isStarConnected I Q
   | P * Q => isStarConnected I P ∧ isStarConnected I Q
-  | RegularExpression.star P => isStarConnected I P ∧ (∀ t ∈ matches_trace I P, @isConnected α I t)
+  | star P => isStarConnected I P ∧ (∀ t ∈ matches_trace I P, @isConnected α I t)
 
 -- Interpretation of this RegularExpression as a <c-rational expression> operating on trace languages.
 def matches_cstar_trace (I : Independence α) : RegularExpression α → Set (Trace I)
   | 0 => {}
   | 1 => {⟦[]⟧}
-  | RegularExpression.char a => {⟦[a]⟧}
+  | char a => {⟦[a]⟧}
   | P + Q => (matches_cstar_trace I P) ∪ (matches_cstar_trace I Q)
   | P * Q => {t | ∃ p : (matches_cstar_trace I P), ∃ q : (matches_cstar_trace I Q), t = p * q}
-  | RegularExpression.star P => kstar (connectedComponents (matches_cstar_trace I P))
+  | star P => kstar (connectedComponents (matches_cstar_trace I P))
 
 /-- Interpreting this RegularExpression as operating on Trace Languages gives the same matching set
   as interpreting (as usual) on String Languages and then projecting to Traces.
@@ -62,7 +62,7 @@ theorem matches_toTrace (P : RegularExpression α) : (matches_trace I P) = toTra
       replace hx : x = [a] := hx
       rw [<- hat, hx]
   | plus P Q ihP ihQ =>
-    unfold matches_trace RegularExpression.matches'
+    unfold matches_trace matches'
     simp [ihP, ihQ]
     apply Set.ext_iff.mpr
     intro t
@@ -83,7 +83,7 @@ theorem matches_toTrace (P : RegularExpression α) : (matches_trace I P) = toTra
       | inl hx => apply Or.inl; use x
       | inr hx => apply Or.inr; use x
   | comp P Q ihP ihQ =>
-    unfold matches_trace RegularExpression.matches' toTrace
+    unfold matches_trace matches' toTrace
     rw [Set.image]
     apply Set.ext_iff.mpr
     intro t
@@ -104,7 +104,7 @@ theorem matches_toTrace (P : RegularExpression α) : (matches_trace I P) = toTra
       rw [<- ht, <- hw]
       rfl
   | star P ih =>
-    unfold matches_trace RegularExpression.matches'
+    unfold matches_trace matches'
     rw [kstar_toTrace_commutes, ih]
 
 
@@ -112,8 +112,8 @@ theorem matches_toTrace (P : RegularExpression α) : (matches_trace I P) = toTra
 lemma matches_toTrace_dist (P Q : RegularExpression α) :
     @toTrace α I (P.matches' + Q.matches') = toTrace P.matches' ∪ toTrace Q.matches' := by
   rw [show P.matches' + Q.matches' = (P + Q).matches' from rfl]
-  repeat rw [<- RegularExpression.matches_toTrace]
-  simp [RegularExpression.matches_trace]
+  repeat rw [<- matches_toTrace]
+  simp [matches_trace]
 
 
 end RegularExpression
